61e27d459e986fe74d5508371fed666edca5cd98
Brando <bbaker.sh@gmail.com>
2024-02-10 12:02:38 -0800

    Add unit tests for Flask app and test database setup and teardown

--- test.py
+++ test.py
@@ -1,24 +1,42 @@
+    # FILEPATH: tests.py
 import unittest
-from flask import Flask
-from app import app  # import your Flask app
+from app import app, db
+from models import Users, Posts
 
-class TestApp(unittest.TestCase):
+class FlaskBloglyTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = app
-        self.client = self.app.test_client()
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
-    def test_home(self):
-        response = self.client.get('/')
+        # Set up test database
+        db.create_all()
+
+        # Add a test user and post
+        self.user = Users(first_name='Test', last_name='User')
+        db.session.add(self.user)
+        db.session.commit()
+
+        self.post = Posts(title='Test Post', content='Test Content', user_id=self.user.id)
+        db.session.add(self.post)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_new_post(self):
+        response = self.client.get(f'/{self.user.id}/posts/new')
         self.assertEqual(response.status_code, 200)
 
-    def test_delete(self):
-        # Assuming you have a user with id 1
-        response = self.client.post('/delete', data={'user_id': 1})
+    def test_post(self):
+        response = self.client.get(f'/{self.user.id}/posts/{self.post.id}')
         self.assertEqual(response.status_code, 200)
 
-    def test_new_user(self):
-        response = self.client.get('/new_user')
+    def test_edit_post(self):
+        response = self.client.get(f'/{self.user.id}/posts/{self.post.id}/edit')
         self.assertEqual(response.status_code, 200)
+
+    # Add more tests...
 
 if __name__ == '__main__':
     unittest.main()
