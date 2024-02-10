17908ba087ac8bcb7c7faefcf819270ea558bccb
Brando <bbaker.sh@gmail.com>
2024-02-10 09:55:50 -0800

    added post model

--- models.py
+++ models.py
@@ -27,6 +27,26 @@ class Users(db.Model):
         u = self
         return f'<User {u.id} {u.first_name} {u.last_name} {u.image_url}>'
         
+class Posts(db.Model):
+    """Post model with a user fk"""
+    
+    __tablename__ = 'posts'
+    
+    id = db.Column(db.Integer,
+                     primary_key=True,
+                     autoincrement=True)
+    title = db.Column(db.String(100),
+                      nullable=False)
+    content = db.Column(db.String,
+                        nullable=False)
+    created_at = db.Column(db.DateTime,
+                          nullable=False,
+                          default=datetime.now())
+    db.ForeignKey('users.id')
+    
+    users = db.relationship('Users', backref='posts')
+        
+        
 def connect_db(app):
     """Connect to database."""
 
